84c1a7cb402d0662c9bbb17781b1b7485e5481f7
Henrik Binggl <henrik@binggl.net>
2018-01-04 18:39:45 +0100

    Love Timezones!

--- Api/src/test/kotlin/net/binggl/mydms/features/upload/UploadModelTest.kt
+++ Api/src/test/kotlin/net/binggl/mydms/features/upload/UploadModelTest.kt
@@ -3,29 +3,28 @@ package net.binggl.mydms.features.upload
 import FixtureHelpers
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import net.binggl.mydms.features.upload.models.UploadItem
-import org.joda.time.format.DateTimeFormat
+import org.joda.time.format.ISODateTimeFormat
 import org.junit.Assert
 import org.junit.Test
-import java.text.SimpleDateFormat
 
 class UploadModelTest {
 
     @Test
     fun serializeToJson(): Unit {
-        val uploadItem = UploadItem("id", "test", "mime", fmt.parseDateTime("20180101").toDate())
+        val uploadItem = UploadItem("id", "test", "mime", fmt.parseDateTime("2018-01-01T00:00:00.000+01:00").toDate())
         val expected = MAPPER.writeValueAsString(MAPPER.readValue(FixtureHelpers.fixture("fixtures/UploadItem.json"), UploadItem::class.java))
         Assert.assertEquals(expected, MAPPER.writeValueAsString(uploadItem))
     }
 
     @Test
     fun deserializeFromJson(): Unit {
-        val uploadItem = UploadItem("id", "test", "mime", fmt.parseDateTime("20180101").toDate())
+        val uploadItem = UploadItem("id", "test", "mime", fmt.parseDateTime("2018-01-01T00:00:00.000+01:00").toDate())
         Assert.assertEquals(MAPPER.readValue(FixtureHelpers.fixture("fixtures/UploadItem.json"), UploadItem::class.java), uploadItem)
     }
 
     companion object {
         // https://github.com/FasterXML/jackson-module-kotlin
         private val MAPPER = jacksonObjectMapper()
-        private val fmt = DateTimeFormat.forPattern("yyyyMMdd")
+        private val fmt =  ISODateTimeFormat.dateTimeParser()
     }
 }
