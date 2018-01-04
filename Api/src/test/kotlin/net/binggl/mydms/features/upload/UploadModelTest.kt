29aa31ee66aec7803d045b342b28e8e33125fda0
Henrik Binggl <henrik@binggl.net>
2018-01-04 18:20:06 +0100

    Use jodatime to parse dates.

--- Api/src/test/kotlin/net/binggl/mydms/features/upload/UploadModelTest.kt
+++ Api/src/test/kotlin/net/binggl/mydms/features/upload/UploadModelTest.kt
@@ -3,6 +3,7 @@ package net.binggl.mydms.features.upload
 import FixtureHelpers
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import net.binggl.mydms.features.upload.models.UploadItem
+import org.joda.time.format.DateTimeFormat
 import org.junit.Assert
 import org.junit.Test
 import java.text.SimpleDateFormat
@@ -11,20 +12,20 @@ class UploadModelTest {
 
     @Test
     fun serializeToJson(): Unit {
-        val uploadItem = UploadItem("id", "test", "mime", simpleDateFormat.parse("01.01.2018"))
+        val uploadItem = UploadItem("id", "test", "mime", fmt.parseDateTime("20180101").toDate())
         val expected = MAPPER.writeValueAsString(MAPPER.readValue(FixtureHelpers.fixture("fixtures/UploadItem.json"), UploadItem::class.java))
         Assert.assertEquals(expected, MAPPER.writeValueAsString(uploadItem))
     }
 
     @Test
     fun deserializeFromJson(): Unit {
-        val uploadItem = UploadItem("id", "test", "mime", simpleDateFormat.parse("01.01.2018"))
+        val uploadItem = UploadItem("id", "test", "mime", fmt.parseDateTime("20180101").toDate())
         Assert.assertEquals(MAPPER.readValue(FixtureHelpers.fixture("fixtures/UploadItem.json"), UploadItem::class.java), uploadItem)
     }
 
     companion object {
         // https://github.com/FasterXML/jackson-module-kotlin
         private val MAPPER = jacksonObjectMapper()
-        private val simpleDateFormat = SimpleDateFormat("dd.MM.yyyy")
+        private val fmt = DateTimeFormat.forPattern("yyyyMMdd")
     }
 }
