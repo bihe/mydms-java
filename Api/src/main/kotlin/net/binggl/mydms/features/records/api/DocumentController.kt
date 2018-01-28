70e80b5b9809475e9d264a95697ee3626e53e3ef
Henrik Binggl <henrik@binggl.net>
2018-01-28 19:46:59 +0100

    Document API save

--- Api/src/main/kotlin/net/binggl/mydms/features/records/api/DocumentController.kt
+++ Api/src/main/kotlin/net/binggl/mydms/features/records/api/DocumentController.kt
@@ -1,15 +1,29 @@
 package net.binggl.mydms.features.records.api
 
+import net.binggl.mydms.features.gdrive.GDriveFileService
+import net.binggl.mydms.features.records.entity.DocumentEntity
+import net.binggl.mydms.features.records.entity.SenderEntity
+import net.binggl.mydms.features.records.entity.TagEntity
 import net.binggl.mydms.features.records.model.Document
 import net.binggl.mydms.features.records.model.OrderBy
 import net.binggl.mydms.features.records.model.PagedDocuments
 import net.binggl.mydms.features.records.model.SortOrder
 import net.binggl.mydms.features.records.repository.DocumentRepository
+import net.binggl.mydms.features.records.repository.SenderRepository
+import net.binggl.mydms.features.records.repository.TagRepository
+import net.binggl.mydms.features.upload.UploadConfig
+import net.binggl.mydms.features.upload.repository.UploadRepository
+import net.binggl.mydms.infrastructure.error.MydmsException
 import net.binggl.mydms.infrastructure.security.ApiSecured
 import net.binggl.mydms.shared.api.BaseResource
+import net.binggl.mydms.shared.files.FileItem
 import net.binggl.mydms.shared.models.ActionResult
 import net.binggl.mydms.shared.models.Role
 import net.binggl.mydms.shared.models.SimpleResult
+import org.apache.commons.io.FileUtils
+import org.apache.commons.io.FilenameUtils
+import org.apache.commons.lang3.RandomStringUtils
+import org.apache.commons.lang3.StringUtils
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
@@ -18,16 +32,26 @@ import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.*
+import java.io.File
+import java.nio.file.FileSystems
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeParseException
 import java.util.*
+import javax.validation.Valid
+import javax.validation.constraints.NotNull
+
 
 @RestController
 @RequestMapping("/api/v1/documents")
 class DocumentController(
         @Value("\${application.defaultLimit}") private val defaultLimitValue: Int,
-        @Autowired private val repository: DocumentRepository): BaseResource() {
+        @Autowired private val uploadConfig: UploadConfig,
+        @Autowired private val repository: DocumentRepository,
+        @Autowired private val tagRepository: TagRepository,
+        @Autowired private val senderRepository: SenderRepository,
+        @Autowired private val uploadRepository: UploadRepository,
+        @Autowired private val fileService: GDriveFileService): BaseResource() {
 
     @ApiSecured(requiredRole = Role.User)
     @GetMapping(produces = ["application/json"])
@@ -128,6 +152,143 @@ class DocumentController(
                     .contentType(MediaType.APPLICATION_JSON)
                     .body(this.error("Could not delete document with id '$id'!"))
         }
+    }
+
+    @ApiSecured(requiredRole = Role.User)
+    @PostMapping(consumes = ["application/json"], produces = ["application/json"])
+    @Transactional()
+    fun saveDocument(@NotNull @Valid documentPayload: Document): ResponseEntity<SimpleResult> {
+
+        val tagList = this.processTags(documentPayload.tags)
+        val senderList = this.processSenders(documentPayload.senders)
+        val fileName = this.processUploadFile(documentPayload.uploadFileToken, documentPayload.fileName)
+        var newDoc = true
+
+        val document = if (StringUtils.isEmpty(documentPayload.id)) {
+            LOG.debug("No id supplied - create a new document.")
+            this.newDocumentInstance(documentPayload, fileName, tagList, senderList)
+        } else {
+            LOG.debug("Id available - lookup document with id '${documentPayload.id}'")
+            val doc = this.repository.findById(documentPayload.id)
+            if (doc.isPresent) {
+                LOG.debug("Got document with id '${documentPayload.id}'")
+                newDoc = false
+                doc.get().copy(title = documentPayload.title,
+                        fileName = fileName,
+                        previewLink = documentPayload.previewLink,
+                        amount = documentPayload.amount,
+                        modified = LocalDateTime.now(),
+                        tags = tagList,
+                        senders = senderList,
+                        senderList = senderList.map { it.name }.joinToString { ";" },
+                        tagList = tagList.map { it.name }.joinToString { ";" })
+            } else {
+                LOG.debug("No document for id '${documentPayload.id}' - create a new document.")
+                this.newDocumentInstance(documentPayload, fileName, tagList, senderList)
+            }
+        }
+
+        val saveDocument = this.repository.save(document)
+        val result = if (newDoc) {
+            SimpleResult(message = "Created new document '${saveDocument.title}' (${saveDocument.id})",
+                    result = ActionResult.Created)
+        } else {
+            SimpleResult(message = "Updated existing document '${saveDocument.title}' (${saveDocument.id})",
+                    result = ActionResult.Updated)
+        }
+
+        return ResponseEntity.ok(result)
+
+    }
+
+    private fun processUploadFile(uploadToken: String?, fileName: String): String {
+        if (uploadToken == null || uploadToken.isEmpty())
+            return fileName
+
+        val result = this.uploadRepository.findById(uploadToken)
+        if (!result.isPresent) {
+            throw MydmsException("Could not get an uploaded document with the given correlation token '$uploadToken'!")
+        }
+
+        LOG.debug("Read upload-item from store - id: '${result.get().id}'")
+
+        val folderName = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy_MM_dd"))
+        val uploadFile = this.getFile(fileName, this.uploadConfig.uploadPath, uploadToken)
+        val uploadFileContents = FileUtils.readFileToByteArray(uploadFile)
+
+        LOG.debug("Got upload file '${uploadFile.name}' with payload size '${uploadFileContents.size}'!")
+
+        val fileItem = FileItem(fileName = fileName, mimeType = result.get().mimeType,
+                payload = uploadFileContents.toTypedArray(), folderName = folderName)
+
+        if (!this.fileService.saveFile(fileItem)) {
+            throw MydmsException("Could not save file in backend store '${fileItem.fileName}'")
+        }
+
+        val filePath = "/$folderName/${fileItem.fileName}"
+
+        if (!uploadFile.delete()) {
+            LOG.warn("Could not delete upload file on filesystem! $uploadToken")
+        }
+
+        this.uploadRepository.deleteById(result.get().id)
+        return filePath
+    }
+
+    private fun getFile(fileName: String, uploadPath: String, uploadToken: String): File {
+        val fileExtension = FilenameUtils.getExtension(fileName)
+        val outputPath = FileSystems.getDefault().getPath(uploadPath,
+                String.format("%s.%s", uploadToken, fileExtension))
+        return outputPath.toFile()
+    }
+
+    private fun processTags(tags: List<String>): List<TagEntity> {
+        return if (tags.isNotEmpty()) {
+            tags.map {
+                val result = this.tagRepository.findByNameContainingIgnoreCase(it)
+                if (result.size == 1) {
+                    result[0]
+                } else {
+                    this.tagRepository.save(TagEntity(name = it))
+                }
+            }
+        } else {
+            emptyList()
+        }
+    }
+
+    private fun processSenders(senders: List<String>): List<SenderEntity> {
+        return if (senders.isNotEmpty()) {
+            senders.map {
+                val result = this.senderRepository.findByNameContainingIgnoreCase(it)
+                if (result.isNotEmpty()) {
+                    result[0]
+                } else {
+                    this.senderRepository.save(SenderEntity(name = it))
+                }
+            }
+        } else {
+            emptyList()
+        }
+    }
+
+    private fun newDocumentInstance(documentPayload: Document,
+                                    fileName: String,
+                                    tagList: List<TagEntity>,
+                                    senderList: List<SenderEntity>): DocumentEntity {
+        return DocumentEntity(id = UUID.randomUUID().toString(),
+                title = documentPayload.title,
+                alternativeId = RandomStringUtils.random(8, true, true),
+                fileName = fileName,
+                previewLink = documentPayload.previewLink,
+                amount = documentPayload.amount,
+                created = LocalDateTime.now(),
+                senders = senderList,
+                tags = tagList,
+                modified = null,
+                senderList = senderList.map { it.name }.joinToString { ";" },
+                tagList = tagList.map { it.name }.joinToString { ";" }
+        )
     }
 
     private fun parseDateTime(input: String): Optional<LocalDateTime> {
