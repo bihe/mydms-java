5c8c5be174f0c7b526a92d3bc634311dae55d350
Henrik Binggl <henrik@binggl.net>
2018-02-12 21:11:10 +0100

    Change the logic to fetch documents and related objects.

--- Api/src/main/kotlin/net/binggl/mydms/features/records/repository/DocumentSearchImpl.kt
+++ Api/src/main/kotlin/net/binggl/mydms/features/records/repository/DocumentSearchImpl.kt
@@ -29,10 +29,12 @@ internal class DocumentSearchImpl : DocumentSearch {
         val whereClause = StringBuilder()
         val joinStatement = StringBuilder()
         val whereDefault = " where 1=1 "
-        var queryString = "select d from DocumentEntity d "
+        val orderBy = this.getDocumentOrderBy("d", *order)
+        var queryString = "select d.id from DocumentEntity d "
         var queryStringCount = "select count(d.id) from DocumentEntity d "
         var hasWhere = false
         var hasJoin = false
+
 
         if (title.isPresent) {
             whereClause.append(" and ( lower(d.title) LIKE :title or lower(d.tagList) like :title or lower(d.senderList) like :title )")
@@ -74,45 +76,92 @@ internal class DocumentSearchImpl : DocumentSearch {
         }
 
         // orderBy
-        queryString += " " + this.getDocumentOrderBy("d", *order)
+        queryString += " " + orderBy
 
-        val jpaQuery = this.entityManager.createQuery(queryString, DocumentEntity::class.java)
+        val jpaIdQuery = this.entityManager.createQuery(queryString, String::class.java)
         val jpaCountQuery = this.entityManager.createQuery(queryStringCount)
 
 
         // set the query parameters
 
         if (title.isPresent) {
-            jpaQuery.setParameter("title", "%${title.get().toLowerCase()}%")
+            jpaIdQuery.setParameter("title", "%${title.get().toLowerCase()}%")
             jpaCountQuery.setParameter("title", "%${title.get().toLowerCase()}%")
         }
         if (dateFrom.isPresent) {
-            jpaQuery.setParameter("dateFrom", dateFrom.get())
+            jpaIdQuery.setParameter("dateFrom", dateFrom.get())
             jpaCountQuery.setParameter("dateFrom", dateFrom.get())
         }
         if (dateUntil.isPresent) {
-            jpaQuery.setParameter("dateUntil", dateUntil.get())
+            jpaIdQuery.setParameter("dateUntil", dateUntil.get())
             jpaCountQuery.setParameter("dateUntil", dateUntil.get())
         }
         if (tag.isPresent) {
-            jpaQuery.setParameter("tag", tag.get().toLowerCase())
+            jpaIdQuery.setParameter("tag", tag.get().toLowerCase())
             jpaCountQuery.setParameter("tag", tag.get().toLowerCase())
         }
         if (sender.isPresent) {
-            jpaQuery.setParameter("sender", sender.get().toLowerCase())
+            jpaIdQuery.setParameter("sender", sender.get().toLowerCase())
             jpaCountQuery.setParameter("sender", sender.get().toLowerCase())
         }
 
         // paging
         if (limit.isPresent) {
-            jpaQuery.maxResults = limit.get()
+            jpaIdQuery.maxResults = limit.get()
         }
         if (skip.isPresent) {
-            jpaQuery.firstResult = skip.get()
+            jpaIdQuery.firstResult = skip.get()
         }
 
+        //
+        // the logic performs 3 queries in fact
+        // 1) count the number of entries for the supplied parameters
+        // 2) get a list of ids for the given query (including the filtering parameters)
+        // 3) retrieve the entity/objects for the given ids
+        //
+        // why three queries? the reason is lazy loading of hibernate related relations.
+        // the data-structure contains a M:N relation to tags and to senders.
+        // the query returns documents objects which have a list of tags/senders as a property.
+        //
+        // in the default lazy-loading setup of hibernate once the tags/senders collection is
+        // accessed additional queries to retrieve tags/senders would be executed (on demand, lazily loaded).
+        // This would lead to the following number of queries in total (e.g. for 5 document entries):
+        //      1 count query
+        //      1 query for documents
+        //      10 queries for tags / senders
+        //          per document entity
+        //              1 query for the tags relation
+        //              1 query for the senders relation
+        // 12 queries in total
+        //
+        // The obvious solution to limit the number of queries would be to FETCH the related entities. This
+        // can be done one a model level @see FetchType (https://www.thoughts-on-java.org/entity-mappings-introduction-jpa-fetchtypes/)
+        // or by using the JOIN FETCH syntax
+        //
+        // One problem occurs when using the FETCH syntax in combination with setMaxResults (aka paging).
+        // setMaxResults only limits (like SQL LIMIT) the total number of rows. If we have an eager fetch - a join
+        // to the related values happens, which increases the total number of "rows" returned by the query. Think
+        // of a SQL JOIN - you get more rows for M:N, 1:N relations. Hibernate deals with the mapping of the owner
+        // objects and the related objects. But setMaxResults only works on the pure DB rows.
+        //
+        // The hibernate solution for this is to fetch all entries - and perform the logic/magic of setMaxResults in
+        // memory. Definitely not a desired approach for large datasets.
+        //
+        // Different solutions are possible - for me the simplest would be to apply setMaxResults to a query returning
+        // ids of objects. The JOIN FETCH would then be applied to another query. This results in a additional query,
+        // but this is a good trade off compared the the default lazy-loading, or doing paging in memory.
+        //
+        // credit to: https://www.tikalk.com/java/blog/hibernate-setmaxresults-join-tables/
+        //
+
         val count = jpaCountQuery.singleResult // get the total number of results
-        val result = jpaQuery.resultList
+        val resultIds = jpaIdQuery.resultList
+
+        // fetch the real document entries for the given ids
+        val entityQueryString = "select d from DocumentEntity d join fetch d.tags t join fetch d.senders s where d.id in :idList $orderBy"
+        val jpaEntityQuery = this.entityManager.createQuery(entityQueryString, DocumentEntity::class.java)
+        jpaEntityQuery.setParameter("idList", resultIds)
+        val result = jpaEntityQuery.resultList
 
         return PagedDocuments(documents = result.map {
             Document(
